ddd787cb7ebdfa15d378f2b61bda89d662fe63a9
jeff-901 <b07901052@ntu.edu.tw>
2024-03-13 21:29:36 -0700

    journal transformation

--- pems.py
+++ pems.py
@@ -27,3 +27,29 @@ class PeMSSample(nx.Sample):
     timestamp: datetime = nx.timestamp()
     average_speed: Speed = nx.data(ge=0)
     total_flow: Flow = nx.data(ge=0)
+
+MINUTE = 60 * 60
+
+class PeMSJournal(nx.Journal):
+    city_id: int = nx.key()
+    timestamp: datetime = nx.timestamp()
+    average_speed: Speed = nx.data(ge=0)
+    total_flow: Flow = nx.data(ge=0)
+
+    def from_sample(self, pemslist, timestamp):
+        for pems in sorted(pemslist):
+            if pems.timestamp <= timestamp + 5 * MINUTE and \
+                pems.timestamp >= timestamp - 5 * MINUTE:
+                self.average_speed = pems.average_speed
+                self.total_flow = pems.total_flow
+
+class SegmentPeMsJournal(nx.Journal):
+    segment_id: int = nx.key()
+    timestamp: datetime = nx.timestamp()
+    average_speed: Speed = nx.data(ge=0)
+    total_flow: Flow = nx.data(ge=0)
+
+    def from_pems_journal(self, pems_journal_list, coefficients):
+        for pems, coef in zip(pems_journal_list, coefficients):
+            self.average_speed += coef * pems.average_speed
+            self.total_flow = coef * pems.total_flow
--- weather.py
+++ weather.py
@@ -35,4 +35,19 @@ class Weather(nx.Sample):
     @get_request
     def from_source(city: City):
         return f"https://api.openweathermap.org/data/2.5/weather?lat={city.lat}&lon={city.lon}&appid={os.environ['API_KEY']}"
+    
 
+MINUTE = 60 * 60
+
+class WeatherJournal(nx.Journal):
+    city_id: int = nx.key()
+    timestamp: datetime = nx.timestamp()
+    visibility: Visibility = nx.data()
+    condition: Condition = nx.data()
+
+    def from_sample(self, weatherlist, timestamp):
+        for weather in sorted(weatherlist):
+            if weather.timestamp <= timestamp + 5 * MINUTE and \
+                weather.timestamp >= timestamp - 5 * MINUTE:
+                self.visibility = weather.visibility
+                self.condition = weather.visibility
